9ef16de4b5b09aed81215a7ca8bc284afd58bd10
Paul Hulett <paulhulett@gmail.com>
2015-11-29 18:11:06 -0800

    stream interface and tests implemented

--- src/streamjson.php
+++ src/streamjson.php
@@ -1,6 +1,248 @@
 <?php
 namespace PaulJulio\StreamJSON;
 
-final class StreamJSON {
+final class StreamJSON implements \Psr\Http\Message\StreamInterface {
+    /* todo: most likely breaks horribly for multi-byte character sets */
 
+    /* @var resource */
+    private $stream;
+    /* @var int the length of the internal json string */
+    private $flen;
+    /* @var int the current pointer position */
+    private $cursor = 0;
+
+    public function __construct() {
+        $this->stream = $this->createStream();
+        fwrite($this->stream, '{}');
+        $this->flen = 2;
+        $this->seek(1);
+    }
+
+    protected function createStream() {
+        $fn = tempnam(sys_get_temp_dir(), 'stream');
+        return fopen('php://temp/' . $fn, 'w');
+    }
+    /**
+     * Reads all data from the stream into a string, from the beginning to end.
+     *
+     * This method MUST attempt to seek to the beginning of the stream before
+     * reading data and read the stream until the end is reached.
+     *
+     * Warning: This could attempt to load a large amount of data into memory.
+     *
+     * This method MUST NOT raise an exception in order to conform with PHP's
+     * string casting operations.
+     *
+     * @see http://php.net/manual/en/language.oop5.magic.php#object.tostring
+     * @return string
+     */
+    public function __toString() {
+        $this->rewind();
+        $this->cursor = $this->flen;
+        return fread($this->stream, $this->flen);
+    }
+    /**
+     * Closes the stream and any underlying resources.
+     *
+     * @return void
+     */
+    public function close() {
+        fclose($this->stream);
+        $this->stream = null;
+    }
+    /**
+     * Separates any underlying resources from the stream.
+     *
+     * After the stream has been detached, the stream is in an unusable state.
+     *
+     * @return resource|null Underlying PHP stream, if any
+     */
+    public function detach() {
+        $stream = $this->stream;
+        $this->stream = null;
+        return $stream;
+    }
+    /**
+     * Get the size of the stream if known.
+     *
+     * @return int|null Returns the size in bytes if known, or null if unknown.
+     */
+    public function getSize() {
+        return $this->flen;
+    }
+    /**
+     * Returns the current position of the file read/write pointer
+     *
+     * @return int Position of the file pointer
+     * @throws \RuntimeException on error.
+     */
+    public function tell() {
+        if (!isset($this->stream)) {
+            throw new \RuntimeException('No stream, no cursor position to report.');
+        }
+        return $this->cursor;
+    }
+    /**
+     * Returns true if the stream is at the end of the stream.
+     *
+     * @return bool
+     */
+    public function eof() {
+        return ($this->cursor == $this->flen);
+    }
+    /**
+     * Returns whether or not the stream is seekable.
+     *
+     * @return bool
+     */
+    public function isSeekable() {
+        return isset($this->stream);
+    }
+    /**
+     * Seek to a position in the stream.
+     *
+     * @link http://www.php.net/manual/en/function.fseek.php
+     * @param int $offset Stream offset
+     * @param int $whence Specifies how the cursor position will be calculated
+     *     based on the seek offset. Valid values are identical to the built-in
+     *     PHP $whence values for `fseek()`.  SEEK_SET: Set position equal to
+     *     offset bytes SEEK_CUR: Set position to current location plus offset
+     *     SEEK_END: Set position to end-of-stream plus offset.
+     * @throws \RuntimeException on failure.
+     */
+    public function seek($offset, $whence = SEEK_SET) {
+        if (!$this->isSeekable()) {
+            throw new \RuntimeException('No stream to seek');
+        }
+        fseek($this->stream, $offset, $whence);
+        switch ($whence) {
+            case SEEK_SET:
+                $this->cursor = $offset;
+                break;
+            case SEEK_CUR:
+                $this->cursor += $offset;
+                break;
+            case SEEK_END:
+                $this->cursor = $this->flen + $offset;
+                break;
+            default:
+                throw new \RuntimeException('Incorrect constant passed for whence');
+        }
+    }
+    /**
+     * Seek to the beginning of the stream.
+     *
+     * If the stream is not seekable, this method will raise an exception;
+     * otherwise, it will perform a seek(0).
+     *
+     * @see seek()
+     * @link http://www.php.net/manual/en/function.fseek.php
+     * @throws \RuntimeException on failure.
+     */
+    public function rewind() {
+        $this->seek(0);
+    }
+    /**
+     * Returns whether or not the stream is writable.
+     *
+     * @return bool
+     */
+    public function isWritable() {
+        return isset($this->stream);
+    }
+    /**
+     * Write data to the stream via an insert.
+     *
+     * This stream is meant to be used as an in-memory json object, overwriting a portion of the stream that has
+     * already been written does not appear to have a meaningful use case.
+     *
+     * @param string $string The string that is to be written.
+     * @return int Returns the number of bytes written to the stream.
+     * @throws \RuntimeException on failure.
+     */
+    public function write($string) {
+        if (!$this->isWritable()) {
+            throw new \RuntimeException('Stream is not writable');
+        }
+        if (!$this->eof()) {
+            // copy the data after the cursor to another stream so that it can be written back
+            $tempStream = $this->createStream();
+            stream_copy_to_stream($this->stream, $tempStream);
+            // reset cursors to their positions prior to the reading
+            fseek($tempStream, 0);
+            $this->seek($this->cursor);
+        }
+        $retval = fwrite($this->stream, $string);
+        if ($retval === false) {
+            throw new \RuntimeException('Write failed (fwrite returned false)');
+        }
+        $this->flen += $retval;
+        $this->cursor += $retval;
+        if (isset($tempStream)) {
+            stream_copy_to_stream($tempStream, $this->stream);
+            fclose($tempStream);
+            $this->seek($this->cursor);
+        }
+        return $retval;
+    }
+    /**
+     * Returns whether or not the stream is readable.
+     *
+     * @return bool
+     */
+    public function isReadable() {
+        return isset($this->stream);
+    }
+    /**
+     * Read data from the stream.
+     *
+     * @param int $length Read up to $length bytes from the object and return
+     *     them. Fewer than $length bytes may be returned if underlying stream
+     *     call returns fewer bytes.
+     * @return string Returns the data read from the stream, or an empty string
+     *     if no bytes are available.
+     * @throws \RuntimeException if an error occurs.
+     */
+    public function read($length) {
+        if (!$this->isReadable()) {
+            throw new \RuntimeException('Stream is not readable');
+        }
+        $this->cursor += $length;
+        if ($this->cursor > $this->flen) {
+            $this->cursor = $this->flen;
+        }
+        return fread($this->stream, $length);
+    }
+    /**
+     * Returns the remaining contents in a string
+     *
+     * @return string
+     * @throws \RuntimeException if unable to read or an error occurs while
+     *     reading.
+     */
+    public function getContents() {
+        return $this->read($this->flen - $this->cursor);
+    }
+    /**
+     * Get stream metadata as an associative array or retrieve a specific key.
+     *
+     * The keys returned are identical to the keys returned from PHP's
+     * stream_get_meta_data() function.
+     *
+     * @link http://php.net/manual/en/function.stream-get-meta-data.php
+     * @param string $key Specific metadata to retrieve.
+     * @return array|mixed|null Returns an associative array if no key is
+     *     provided. Returns a specific key value if a key is provided and the
+     *     value is found, or null if the key is not found.
+     */
+    public function getMetadata($key = null) {
+        $md = stream_get_meta_data($this->stream);
+        if (isset($key)) {
+            if (isset($md[$key])) {
+                return $md[$key];
+            }
+            return null;
+        }
+        return $md;
+    }
 }
--- test/unit/StreamJSONTest.php
+++ test/unit/StreamJSONTest.php
@@ -2,7 +2,138 @@
 
 class StreamJSONTest extends PHPUnit_Framework_TestCase {
 
+    /* @var \PaulJulio\StreamJSON\StreamJSON */
+    private $sj;
+
+    public function setUp() {
+        parent::setUp();
+        $this->createInstance();
+    }
+
+    protected function createInstance() {
+        $this->sj = new \PaulJulio\StreamJSON\StreamJSON();
+    }
+
     public function testStructure() {
-        $this->assertClassHasAttribute('testable', PaulJulio\StreamJSON\StreamJSON::class);
+        $this->assertClassHasAttribute('stream', PaulJulio\StreamJSON\StreamJSON::class);
+        $this->assertClassHasAttribute('flen', PaulJulio\StreamJSON\StreamJSON::class);
+        $this->assertClassHasAttribute('cursor', PaulJulio\StreamJSON\StreamJSON::class);
+    }
+
+    public function testEmpty() {
+        $this->assertEquals(1, $this->sj->tell(), 'empty object pointer is at correct position');
+        $this->assertEquals('{}', (string) $this->sj, 'empty object casts to string as empty json object');
+        $this->assertTrue($this->sj->eof(), 'empty object is at end of file after read');
+        $this->assertEquals(2, $this->sj->getSize(), 'empty object has correct size');
+        $this->sj->rewind();
+        $this->assertEquals(0, $this->sj->tell(), 'empty object pointer is at correct position after rewind');
+        $fp = $this->sj->detach();
+        $this->assertEquals('{}', fread($fp, 2), 'reading results of detach results in the json empty object');
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testDetachExceptionTell() {
+        $this->assertTrue($this->sj->isSeekable());
+        $this->sj->detach();
+        $this->assertFalse($this->sj->isSeekable());
+        $this->sj->tell();
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testSeekEnd() {
+        $this->sj->seek(0, SEEK_END);
+        $this->assertEquals(2, $this->sj->tell());
+        $this->createInstance();
+        $this->sj->seek(-1, SEEK_END);
+        $this->assertEquals(1, $this->sj->tell());
+        $fp = $this->sj->detach();
+        $this->assertEquals('}', fread($fp,1));
+        $this->sj->seek(0, SEEK_END);
+    }
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testSeekCur() {
+        $this->sj->seek(1, SEEK_CUR);
+        $this->assertEquals(2, $this->sj->tell());
+        $this->sj->seek(-1, SEEK_CUR);
+        $this->assertEquals(1, $this->sj->tell());
+        $fp = $this->sj->detach();
+        $this->assertEquals('}', fread($fp,1));
+        $this->sj->seek(0, SEEK_CUR);
+    }
+
+    public function testIsWritable() {
+        $this->assertTrue($this->sj->isWritable());
+        $this->sj->detach();
+        $this->assertFalse($this->sj->isWritable());
+    }
+
+    public function testIsReadable() {
+        $this->assertTrue($this->sj->isReadable());
+        $this->sj->detach();
+        $this->assertFalse($this->sj->isReadable());
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testWrite() {
+        $compare = '"foo":"bar"';
+        $this->sj->seek(1);
+        $retval = $this->sj->write($compare);
+        $this->assertEquals(strlen($compare), $retval);
+        $this->assertEquals(1 + $retval, $this->sj->tell());
+        $compare = (string) $this->sj;
+        $this->assertEquals('{"foo":"bar"}', $compare);
+        $this->assertEquals(strlen($compare), $this->sj->getSize());
+        $this->sj->detach();
+        $this->sj->write('failure');
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testRead() {
+        $compare = '"foo":"bar"';
+        $this->sj->seek(1);
+        $this->sj->write($compare);
+        $this->sj->seek(1);
+        $val = $this->sj->read(strlen($compare));
+        $this->assertEquals($compare, $val);
+        $this->assertEquals(1 + strlen($compare), $this->sj->tell());
+        $this->sj->seek(0);
+        $this->sj->read(50);
+        $this->assertEquals($this->sj->getSize(), $this->sj->tell());
+        $this->sj->detach();
+        $this->sj->read(1);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testGetContents() {
+        $compare = '"foo":"bar"';
+        $this->sj->seek(1);
+        $this->sj->write($compare);
+        $this->sj->seek(-6, SEEK_END);
+        $this->assertEquals('"bar"}', $this->sj->getContents());
+        $this->sj->detach();
+        $this->sj->getContents();
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testRewind() {
+        $this->sj->write('test');
+        $this->sj->rewind();
+        $this->assertEquals(0, $this->sj->tell());
+        $this->sj->detach();
+        $this->sj->rewind();
     }
 }
